6d249f46b6dce68741665adb7fd280f98d3a1ab2
Jones Cian <179418119+CianCode@users.noreply.github.com>
2025-12-13 18:04:50 +0100

    🎉 Start the redlife fivem server core

---
+++ config/global.lua
@@ -0,0 +1,18 @@
+--[[
+  This file is part of RedLife.
+  Created at 12/12/2025 23:15
+
+  Copyright (c) RedLife - All Rights Reserved
+
+  Unauthorized using, copying, modifying and/or distributing of this file,
+  via any medium is strictly prohibited. This code is confidential.
+--]]
+---@author CianCode
+
+_Config = {
+    prefix = "[^1RedLife^7]",
+    version = "Alpha",
+    environment = "DEV",
+    enableErrorsLog = true,
+    enableSqlLog = true,
+}
---
+++ src/web/src/App.tsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <h1>RedLife Roleplay</h1>
+    </div>
+  );
+}
+
+export default App;
