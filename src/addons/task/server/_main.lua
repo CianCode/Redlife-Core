4dc3d424134a7082152af36672468067b1ee1caf
Jones Cian <179418119+CianCode@users.noreply.github.com>
2026-01-02 16:51:51 +0100

    🚀 Adding blips and also notifiations web.

---
+++ src/addons/task/server/_main.lua
@@ -0,0 +1,13 @@
+--[[
+  This file is part of Redlife.
+  Created at 02/01/2026 16:50
+
+  Copyright (c) Redlife - All Rights Reserved
+
+  Unauthorized using, copying, modifying and/or distributing of this file,
+  via any medium is strictly prohibited. This code is confidential.
+--]]
+---@author CianCode
+
+---@class _RedServer_Task
+_RedServer_Task = {}
---
+++ src/addons/task/server/blipUpdater.lua
@@ -0,0 +1,71 @@
+--[[
+  This file is part of Redlife.
+  Created at 02/01/2026 16:50
+
+  Copyright (c) Redlife - All Rights Reserved
+
+  Unauthorized using, copying, modifying and/or distributing of this file,
+  via any medium is strictly prohibited. This code is confidential.
+--]]
+---@author CianCode
+
+_RedServer_Task.doBlipUpdater = function()
+    ---@param player _Player
+    for _src, player in pairs(_RedServer_Players.getAll()) do
+        if (not (player.spawned)) then
+            goto skipPlayer
+        end
+        local playerCoords = GetEntityCoords(GetPlayerPed(_src))
+        ---@param blip _Blip
+        for _, blip in pairs(_RedServer_Blips.getAll()) do
+            local distance = #(playerCoords - blip.position)
+            if (blip.restricted) then
+                --[[
+                    Restricted zones
+                --]]
+                if (blip:isAllowed(_src)) then
+                    if (not (blip:isSubscribed(_src)) and blip:isEverywhere()) then
+                        blip:subscribe(_src)
+                        goto continue
+                    end
+
+                    if (not (blip:isSubscribed(_src)) and not (blip:isEverywhere()) and distance <= blip.drawDist) then
+                        blip:subscribe(_src)
+                        goto continue
+                    end
+
+                    if (blip:isSubscribed(_src) and not (blip:isEverywhere()) and distance > blip.drawDist) then
+                        blip:unsubscribe(_src)
+                        goto continue
+                    end
+                else
+                    if (blip:isSubscribed(_src)) then
+                        --blip:unsubscribe(_src)
+                        goto continue
+                    end
+                end
+            end
+
+            --[[
+                Public zones
+            --]]
+            if (blip:isEverywhere() and not (blip:isSubscribed(_src))) then
+                blip:subscribe(_src)
+                goto continue
+            end
+
+            if (not (blip:isEverywhere()) and blip:isSubscribed(_src) and not (distance <= blip.drawDist)) then
+                blip:unsubscribe(_src)
+                goto continue
+            end
+
+            if (not (blip:isEverywhere()) and not (blip:isSubscribed(_src)) and distance <= blip.drawDist) then
+                blip:subscribe(_src)
+                goto continue
+            end
+
+            :: continue ::
+        end
+        :: skipPlayer ::
+    end
+end
